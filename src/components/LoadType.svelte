e7fdbcc6c0f31c3978f79e71ea877b5daa3eef7b
Dhruv Kaushik <dhruv.kaushik0009@gmail.com>
2025-03-14 17:32:48 +0530

    final changes

--- src/components/LoadType.svelte
+++ src/components/LoadType.svelte
@@ -214,7 +214,7 @@ A screen bigger than 800x600 is recommended
       <img src="plus.svg" alt="plus icon" />
       <span>Upload your own .typ</span>
     </button>
-    <a href="https://github.com/dhruvvkaushikx/DevKeys" target="_blank">
+    <a href="https://github.com/dhruvvkaushik/DevKeys" target="_blank">
       <button class="scriptButton more" style="background-color: #004b53; box-shadow: 0px 0px 15px -3px #004b53">
         <!-- <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-plus"><line x1="12" y1="5" x2="12" y2="19"></line><line x1="5" y1="12" x2="19" y2="12"></line></svg> -->
         <img src="github.svg" alt="github icon" />
